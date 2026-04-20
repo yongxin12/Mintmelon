5dddebbc51245cbe7c541b4f100aeea6fec6bb39
RZ <yongxin1@ualberta.ca>
2026-04-20 13:23:11 -0700

    deploy: publish static site to pages

---
+++ _next/static/chunks/app/[locale]/not-found-db198338a9db4318.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[224,345],{8691:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4256,23))}},e=>{e.O(0,[256,623,338,358],()=>e(e.s=8691)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/layout-0ff689fce91bcc58.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2950:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7497,23))},7497:()=>{}},e=>{e.O(0,[766,623,338,358],()=>e(e.s=2950)),_N_E=e.O()}]);
